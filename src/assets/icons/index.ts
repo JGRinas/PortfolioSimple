44bad6963abd7531e903d5b5c76c597947f459d8
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-03-19 00:02:20 -0300

    configure vite-plugin-svgr

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,34 +1,36 @@
+/// <reference types="vite-plugin-svgr/client" />
+
 //Maquetación y estilos
-export { default as HtmlIcon } from "./html.svg";
-export { default as CssIcon } from "./css.svg";
+export { default as HtmlIcon } from "./html.svg?react";
+export { default as CssIcon } from "./css.svg?react";
 
 //Lenguajes de programación
-export { default as CSharpIcon } from "./c-sharp.svg";
-export { default as JavaScriptIcon } from "./javascript.svg";
-export { default as TypeScriptIcon } from "./typescript.svg";
-export { default as PhpIcon } from "./php.svg";
+export { default as CSharpIcon } from "./c-sharp.svg?react";
+export { default as JavaScriptIcon } from "./javascript.svg?react";
+export { default as TypeScriptIcon } from "./typescript.svg?react";
+export { default as PhpIcon } from "./php.svg?react";
 
 //Frameworks y librerías FrontEnd
-export { default as BootstrapIcon } from "./bootstrap.svg";
-export { default as TailwindIcon } from "./tailwind.svg";
-export { default as ReactIcon } from "./react.svg";
-export { default as ViteIcon } from "./vitejs.svg";
-export { default as NextIcon } from "./nextjs.svg";
+export { default as BootstrapIcon } from "./bootstrap.svg?react";
+export { default as TailwindIcon } from "./tailwind.svg?react";
+export { default as ReactIcon } from "./react.svg?react";
+export { default as ViteIcon } from "./vitejs.svg?react";
+export { default as NextIcon } from "./nextjs.svg?react";
 
 //Frameworks y librerías BackEnd
-export { default as NodeIcon } from "./nodejs.svg";
-export { default as ExpressIcon } from "./express.svg";
-export { default as CodeIgniterIcon } from "./codeigniter.svg";
-export { default as LaravelIcon } from "./laravel.svg";
+export { default as NodeIcon } from "./nodejs.svg?react";
+export { default as ExpressIcon } from "./express.svg?react";
+export { default as CodeIgniterIcon } from "./codeigniter.svg?react";
+export { default as LaravelIcon } from "./laravel.svg?react";
 
 //Herramientas de versionado y repositorios
-export { default as GitIcon } from "./git.svg";
-export { default as GitHubIcon } from "./github.svg";
-export { default as GitLabIcon } from "./gitlab.svg";
+export { default as GitIcon } from "./git.svg?react";
+export { default as GitHubIcon } from "./github.svg?react";
+export { default as GitLabIcon } from "./gitlab.svg?react";
 
 //Herramientas para desarrollo desktop
-export { default as ElectronIcon } from "./electronjs.svg";
+export { default as ElectronIcon } from "./electronjs.svg?react";
 
 //Sistemas operativos
-export { default as UbuntuIcon } from "./ubuntu.svg";
-export { default as WindowsIcon } from "./windows.svg";
+export { default as UbuntuIcon } from "./ubuntu.svg?react";
+export { default as WindowsIcon } from "./windows.svg?react";
