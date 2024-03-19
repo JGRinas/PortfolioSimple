ba451757825e23870d2bfc49ab71f4cd87dbb997
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-03-19 00:01:11 -0300

    added icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,34 @@
+//Maquetación y estilos
+export { default as HtmlIcon } from "./html.svg";
+export { default as CssIcon } from "./css.svg";
+
+//Lenguajes de programación
+export { default as CSharpIcon } from "./c-sharp.svg";
+export { default as JavaScriptIcon } from "./javascript.svg";
+export { default as TypeScriptIcon } from "./typescript.svg";
+export { default as PhpIcon } from "./php.svg";
+
+//Frameworks y librerías FrontEnd
+export { default as BootstrapIcon } from "./bootstrap.svg";
+export { default as TailwindIcon } from "./tailwind.svg";
+export { default as ReactIcon } from "./react.svg";
+export { default as ViteIcon } from "./vitejs.svg";
+export { default as NextIcon } from "./nextjs.svg";
+
+//Frameworks y librerías BackEnd
+export { default as NodeIcon } from "./nodejs.svg";
+export { default as ExpressIcon } from "./express.svg";
+export { default as CodeIgniterIcon } from "./codeigniter.svg";
+export { default as LaravelIcon } from "./laravel.svg";
+
+//Herramientas de versionado y repositorios
+export { default as GitIcon } from "./git.svg";
+export { default as GitHubIcon } from "./github.svg";
+export { default as GitLabIcon } from "./gitlab.svg";
+
+//Herramientas para desarrollo desktop
+export { default as ElectronIcon } from "./electronjs.svg";
+
+//Sistemas operativos
+export { default as UbuntuIcon } from "./ubuntu.svg";
+export { default as WindowsIcon } from "./windows.svg";
